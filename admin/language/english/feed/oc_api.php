d71e28ed9bef7e56b7197ae429457b93af5d9297
Norbert Feria <norbert.feria@gmail.com>
2014-05-15 23:05:44 +0800

    Create oc_api.php

---
+++ admin/language/english/feed/oc_api.php
@@ -0,0 +1,16 @@
+<?php
+
+$_['heading_title']			= 'OpenCart API';
+
+$_['text_enabled']			= 'Enabled';
+$_['text_disabled']			= 'Disabled';
+$_['text_feed']				= 'Feeds';
+$_['text_success']			= 'Settings saved';
+$_['text_homepage']			= 'OC API Documentation';
+
+$_['tab_general']			= 'General';
+
+$_['entry_status']			= 'Status:';
+$_['entry_appid']			= 'APP I.D.:';
+$_['entry_key']				= 'APP Private Key:';
+$_['entry_pubkey']			= 'APP Public Key:';
